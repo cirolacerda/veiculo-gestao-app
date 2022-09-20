54e2cec0cf1461fff834cff65a9e1ebaaefa3be0
Ciro Botelho Lacerda <cirolacerda@gmail.com>
2022-09-20 15:40:03 -0300

    Criando model motorista

---
+++ app/Models/Motorista.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Motorista extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'nome',
+        'matricula',
+        'registro_habilitacao',
+        'categoria_cnh',
+        'validade_cnh',
+        'primeira_habilitacao',
+        'departamento'
+    ];
+
+}
