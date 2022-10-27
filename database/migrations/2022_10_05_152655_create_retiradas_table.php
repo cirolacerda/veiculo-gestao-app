a71675d08285ceef90d7bfdf68069cd5d0c19a05
Ciro Botelho Lacerda <cirolacerda@gmail.com>
2022-10-27 20:56:00 -0300

    Criando migration tabela retiradas

---
+++ database/migrations/2022_10_05_152655_create_retiradas_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('retiradas', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('veiculo_id');
+            $table->unsignedBigInteger('motorista_id');
+            $table->unsignedBigInteger('departamento_id');
+            $table->date('data_retirada');
+            $table->time('hora_retirada');
+            $table->double('medida_saida');
+            $table->text('destino')->nullable(true);
+            $table->text('observacao')->nullable(true);
+            $table->timestamps();
+
+            //Constraint
+            $table->foreign('veiculo_id')->references('id')->on('veiculos');
+            $table->foreign('motorista_id')->references('id')->on('motoristas');
+            $table->foreign('departamento_id')->references('id')->on('departamentos');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('retiradas');
+    }
+};
