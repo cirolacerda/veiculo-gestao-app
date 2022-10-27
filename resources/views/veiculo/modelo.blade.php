99e7b620595ddd364503f454d7a8dd1ff92fbda6
Ciro Botelho Lacerda <cirolacerda@gmail.com>
2022-10-27 20:57:26 -0300

    Criando view cadastro modelo

---
+++ resources/views/veiculo/modelo.blade.php
@@ -0,0 +1,21 @@
+<h1>Cadastrar Modelo</h1>
+
+<form action={{ route('modelo.cadastrar') }} method="POST">
+    @csrf
+    <br/>
+    <select name="marca_id" id="marca_id">
+        <option value="">Selecione a marca  </option>
+
+        @foreach ($marcas as $marca)
+            <option value="{{ $marca->id }}"> {{ $marca->nome }} </option>
+        @endforeach
+
+    </select>
+    <br/>
+    <br/>
+    <input placeholder="Insira o nome do modelo" name="nome" id="nome" type="text"/>
+    <br/>
+    <br/>
+
+    <button type="submit">Cadastrar</button>
+</form>
