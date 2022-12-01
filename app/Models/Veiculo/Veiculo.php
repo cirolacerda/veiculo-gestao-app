570c932138d002984a18b02dba209066cd986496
Ciro Botelho Lacerda <cirolacerda@gmail.com>
2022-12-01 13:56:09 -0300

    Adicionando relacionamentos model veiculo

--- app/Models/Veiculo/Veiculo.php
+++ app/Models/Veiculo/Veiculo.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Veiculo;
 
+use App\Models\Departamento;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -29,4 +30,44 @@ class Veiculo extends Model
         'capacidade_passageiros'
 
     ];
+
+    public function tipo()
+    {
+        return $this->belongsTo(Tipo::class);
+    }
+
+    public function marca()
+    {
+        return $this->belongsTo(Marca::class);
+    }
+
+    public function modelo()
+    {
+        return $this->belongsTo(Modelo::class);
+    }
+
+    public function cor()
+    {
+        return $this->belongsTo(Cor::class);
+    }
+
+    public function tipoMedida()
+    {
+        return $this->belongsTo(TipoMedida::class);
+    }
+
+    public function combustivel()
+    {
+        return $this->belongsTo(Combustivel::class);
+    }
+
+    public function departamento()
+    {
+        return $this->belongsTo(Departamento::class);
+    }
+
+    public function categoriaCnh()
+    {
+        return $this->belongsTo(CategoriaCnh::class);
+    }
 }
